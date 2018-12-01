f71feb73a36e30f04d02b101dd3c4ba1a887f693
Marcin Chochowski <Marcin.Chochowski@gmail.com>
2018-12-01 21:14:52 +0100

    initial commit

---
+++ src/main/java/pl/chochowski/sfgpetclinic/SfgpetclinicApplication.java
@@ -0,0 +1,12 @@
+package pl.chochowski.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgpetclinicApplication.class, args);
+	}
+}
